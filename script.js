161d24b6186b4c0e5e1d79d6cba9ddb924386e35
dimdimotis112 <normixslv@gmail.com>
2022-10-01 09:18:22 +0300

    add function and event listener to div square so it changes color on hover

--- script.js
+++ script.js
@@ -9,7 +9,13 @@ for (let i = 0; i < 16; i++) {
         const container = document.createElement("div");
         container.style.width = "50px";
         container.style.height = "50px";
+        container.addEventListener("mouseover", changeColor); // So the user can change the color of the square when hovered over.
         rowHolder.appendChild(container);
     } 
     divContainer.appendChild(rowHolder);
+}   
+
+
+function changeColor() {
+    this.style.backgroundColor = "black";
 }
