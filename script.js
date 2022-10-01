25884fd0b2966d81cd601f0cd3107bd828c781ee
dimdimotis112 <normixslv@gmail.com>
2022-10-01 09:26:00 +0300

    make each square a square have a dinamic size

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ for (let i = 0; i < 16; i++) {
     rowHolder.style.justifyContent = "center";
     for (let j = 0; j < 16; j++) {
         const container = document.createElement("div");
-        container.style.width = "50px";
-        container.style.height = "50px";
+        container.style.flex = "1"; // sets width to take up the whole width it can.
+        container.style.aspectRatio = "1 / 1"; // sets squares to be same width and heigth.
         container.addEventListener("mouseover", changeColor); // So the user can change the color of the square when hovered over.
         rowHolder.appendChild(container);
     } 
